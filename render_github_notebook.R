5c20056afedf2ab70dea23321e377c4276e0d273
zouter <wouter.saelens@gmail.com>
2018-08-28 09:30:53 +0200

    remove commands

--- render_github_notebook.R
+++ render_github_notebook.R
@@ -5,7 +5,7 @@ file <- "02-metric_characterisation/README.Rmd"
 tempfile <- fs::path_ext_set(file, "tmp.Rmd")
 file %>% 
   read_lines() %>% 
-  str_replace_all("\\$[^\\$]+\\$", function(x) paste0("![](https://latex.codecogs.com/gif.latex?", URLencode(str_sub(x, 2, -2)), ")")) %>%
+  dynbenchmark::render_equations(format = "markdown") %>%
   write_lines(tempfile)
 render(
   tempfile,
@@ -13,4 +13,6 @@ render(
   output_format = rmarkdown::github_document(toc = TRUE, toc_depth = 1)
 )
 file.remove(tempfile)
+browseURL(fs::path_ext_set(file, "html"))
+Sys.sleep(1)
 file.remove(fs::path_ext_set(file, "html"))
